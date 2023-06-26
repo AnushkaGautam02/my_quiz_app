69981bd2b282ad5180d493644a9d68d316bf3f3e
AnushkaGautam02 <anushka.infoseek@gmail.com>
2023-06-27 01:12:53 +0530

    "questions"

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,62 +1,65 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useFetchQuestion } from '../hooks/FetchQuestion';
+import { updateResult } from '../hooks/setResult';
 
-import {useDispatch, useSelector} from 'react-redux'
+export default function Questions({ onChecked }) {
+  const [checked, setChecked] = useState(undefined);
+  const questionsQueue = useSelector((state) => state.questions.queue);
+  const trace = useSelector((state) => state.questions.trace);
+  const result = useSelector((state) => state.result.result);
+  const [{ isLoading, apiData, serverError }] = useFetchQuestion();
 
-/** Custom Hook*/
-import { useFetchQuestion } from '../hooks/FetchQuestion'
+  const questions = questionsQueue && questionsQueue[trace];
 
-import { updateResult } from '../hooks/setResult'
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(updateResult({ trace, checked }));
+  }, [checked, dispatch, trace]);
 
-export default function Questions(  {onChecked} ) {
+  function onSelect(i) {
+    onChecked(i);
+    setChecked(i);
+  }
 
-    const [checked, setChecked] = useState(undefined)
-    const {trace} = useSelector(state => state.questions)
-    const result = useSelector(state => state.result.result)
-    const [{isLoading, apiData,serverError}] = useFetchQuestion()
+  if (isLoading) {
+    return <h3 className='text-light'>Loading...</h3>;
+  }
 
+  if (serverError) {
+    return (
+      <h3 className='text-light'>{serverError.message || 'Unknown Error'}</h3>
+    );
+  }
 
-    const questions = useSelector( state=> state.questions.queue[state.questions.trace])
-    const dispatch = useDispatch()
-
-    useEffect(() =>{
-        
-       dispatch( updateResult({trace, checked}))
-    }, [checked])
-
-    function onSelect(i){
-        
-      // console.log(i)
-       onChecked(i)
-       setChecked(i)
-    }   dispatch( updateResult({trace, checked}))
-
-
-    if(isLoading) return <h3 className='text-light'>isLoading</h3>
-    if(serverError) return<h3 className='text-light'>{serverError.message || "Unknown Error"}</h3>
+  if (!questions) {
+    return <h3 className='text-light'>No questions available.</h3>;
+  }
 
   return (
-    <div className='questons'>
-        <h2 className='text-light'>
-            {questions?.question}
-        </h2>
-
-        <ul key ={questions?.id}>
-           {
-            questions?.options.map((q,i) =>(
-                <li key={i}>
-                    <input type='radio'
-                        value={false}
-                        name='options'
-                        id={`q${i}-option`}
-                        onChange= {()=> onSelect(i)} />
-
-                    <label className='text-primary' htmlFor={`q${i}-option`}>{q}</label>
-                    <div className={`check ${result[trace] === i ? 'checked': '' }`}></div>
-            </li>
-            ))
-           }
-        </ul>
+    <div className='questions'>
+      <h2 className='text-light'>{questions.question}</h2>
+      <ul>
+        {questions.options.map((q, i) => (
+          <li key={i}>
+            <input
+              type='radio'
+              value={i}
+              name='options'
+              id={`q${i}-option`}
+              onChange={() => onSelect(i)}
+              checked={checked === i}
+            />
+            <label className='text-primary' htmlFor={`q${i}-option`}>
+              {q}
+            </label>
+            <div
+              className={`check ${result[trace] === i ? 'checked' : ''}`}
+            ></div>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
